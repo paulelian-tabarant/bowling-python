257dacbf42a1915b06edf95a7bc437408ca8d42e
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-02-15 16:35:14 +0100

    feat: refactor tests

--- game_test.py
+++ game_test.py
@@ -4,19 +4,21 @@ from game import Game
 
 
 class MyTestCase(unittest.TestCase):
-    def test_has_zero_total_score_when_gutter_ball(self):
-        game = Game()
-        for i in range(20):
-            game.roll(0)
+    def setUp(self):
+        self.game = Game()
 
-        self.assertEqual(0, game.score())
+    def test_has_zero_total_score_when_gutter_ball(self):
+        for roll in range(20):
+            self.game.roll(0)
+
+        self.assertEqual(0, self.game.score())
 
     def test_adds_pins_from_each_roll_when_3(self):
-        game = Game()
         for i in range(20):
-            game.roll(3)
+            self.game.roll(3)
 
-        self.assertEqual(60, game.score())
+        self.assertEqual(60, self.game.score())
+
 
 
 if __name__ == '__main__':
