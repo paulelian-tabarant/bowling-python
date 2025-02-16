e4cb9bd789d2c40ceec3f0806551e9c5f7881007
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-02-16 16:56:04 +0100

    refactor

--- frame.py
+++ frame.py
@@ -4,27 +4,24 @@ MAX_PINS = 10
 class Frame:
     def __init__(self):
         self.first_roll: int or None = None
-        self.second_roll: int or None = None
+        self.__second_roll: int or None = None
 
     def roll(self, pins_down: int):
         if self.first_roll is None:
             self.first_roll = pins_down
             return
 
-        if self.second_roll is None:
-            self.second_roll = pins_down
+        if self.__second_roll is None:
+            self.__second_roll = pins_down
 
     def is_over(self):
         if self.is_strike():
             return True
 
-        return self.first_roll is not None and self.second_roll is not None
+        return self.first_roll is not None and self.__second_roll is not None
 
     def score(self):
-        if self.is_strike():
-            return MAX_PINS
-
-        return self.first_roll + self.second_roll if self.second_roll is not None else self.first_roll
+        return self.first_roll + self.__second_roll if self.__second_roll is not None else self.first_roll
 
     def is_spare(self):
         return self.first_roll != MAX_PINS and self.score() == MAX_PINS
--- game.py
+++ game.py
@@ -10,38 +10,38 @@ class Game:
         self.frames = [Frame()]
 
     def roll(self, pins_down):
-        if self.last_frame().is_over():
+        if self.__last_frame().is_over():
             self.frames.append(Frame())
 
-        self.last_frame().roll(pins_down)
+        self.__last_frame().roll(pins_down)
 
     def score(self):
         result = 0
 
         for frame_number in range(1, len(self.frames) + 1):
-            frame = self.nth_frame(frame_number)
+            frame = self.__nth_frame(frame_number)
 
             if is_not_extra(frame_number) and frame.is_spare():
-                result += self.spare_bonus_at(frame_number)
+                result += self.__spare_bonus_at(frame_number)
 
             if is_not_extra(frame_number) and frame.is_strike():
-                result += self.strike_bonus_at(frame_number)
+                result += self.__strike_bonus_at(frame_number)
 
             result += frame.score()
 
         return result
 
-    def strike_bonus_at(self, n):
-        if self.nth_frame(n + 1).is_strike():
-            return self.nth_frame(n + 1).score() + self.nth_frame(n + 2).first_roll
+    def __strike_bonus_at(self, n):
+        if self.__nth_frame(n + 1).is_strike():
+            return self.__nth_frame(n + 1).score() + self.__nth_frame(n + 2).first_roll
 
-        return self.nth_frame(n + 1).score()
+        return self.__nth_frame(n + 1).score()
 
-    def spare_bonus_at(self, n):
-        return self.nth_frame(n + 1).first_roll
+    def __spare_bonus_at(self, n):
+        return self.__nth_frame(n + 1).first_roll
 
-    def last_frame(self):
+    def __last_frame(self):
         return self.frames[-1]
 
-    def nth_frame(self, number):
+    def __nth_frame(self, number):
         return self.frames[number - 1]
