69c4f2b96f65b37152009fb87882a1bf9efe4f58
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-02-16 12:38:20 +0100

    refactor(wip): introduce frames storage

---
+++ frame.py
@@ -0,0 +1,18 @@
+class Frame:
+    def __init__(self):
+        self.first_roll: int or None = None
+        self.second_roll: int or None = None
+
+    def roll(self, pins_down: int):
+        if self.first_roll is None:
+            self.first_roll = pins_down
+            return
+
+        if self.second_roll is None:
+            self.second_roll = pins_down
+
+    def is_over(self):
+        return self.first_roll is not None and self.second_roll is not None
+
+    def score(self):
+        return self.first_roll + self.second_roll
--- game.py
+++ game.py
@@ -1,21 +1,6 @@
+from frame import Frame
+
 MAX_PINS = 10
-
-
-class Frame:
-    def __init__(self):
-        self.first_roll = None
-        self.second_roll = None
-
-    def roll(self, pins_down):
-        if self.first_roll is None:
-            self.first_roll = pins_down
-            return
-
-        if self.second_roll is None:
-            self.second_roll = pins_down
-
-    def is_over(self):
-        return self.first_roll is not None and self.second_roll is not None
 
 
 class Game:
@@ -26,12 +11,10 @@ class Game:
     def roll(self, param):
         self.rolls.append(param)
 
-        last_frame = self.frames[-1]
-        if not last_frame.is_over():
-            last_frame.roll(param)
-            return
+        if self.frames[-1].is_over():
+            self.frames.append(Frame())
 
-        self.frames.append(Frame())
+        self.frames[-1].roll(param)
 
     def score(self):
         score = 0
@@ -43,6 +26,9 @@ class Game:
             if self.rolls[first_roll] + self.rolls[second_roll] == MAX_PINS:
                 score += self.rolls[second_roll + 1]
 
-            score += self.rolls[first_roll] + self.rolls[second_roll]
+            # score += self.rolls[first_roll] + self.rolls[second_roll]
+
+        for frame in range(len(self.frames)):
+            score += self.frames[frame].score()
 
         return score
