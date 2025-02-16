ebb05268418e2b097a8982b81a6f09628108dea6
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-02-16 19:36:29 +0100

    refactor

--- frame.py
+++ frame.py
@@ -1,27 +1,31 @@
 MAX_PINS = 10
 
 
+def is_over(roll):
+    return roll is not None
+
+
 class Frame:
     def __init__(self):
-        self.first_roll: int or None = None
-        self.__second_roll: int or None = None
+        self.first_roll = None
+        self.__second_roll = None
 
-    def roll(self, pins_down: int):
-        if self.first_roll is None:
+    def roll(self, pins_down):
+        if not is_over(self.first_roll):
             self.first_roll = pins_down
             return
 
-        if self.__second_roll is None:
+        if not is_over(self.__second_roll):
             self.__second_roll = pins_down
 
     def is_over(self):
         if self.is_strike():
             return True
 
-        return self.first_roll is not None and self.__second_roll is not None
+        return is_over(self.first_roll) and is_over(self.__second_roll)
 
     def score(self):
-        return self.first_roll + self.__second_roll if self.__second_roll is not None else self.first_roll
+        return self.first_roll + self.__second_roll if is_over(self.__second_roll) else self.first_roll
 
     def is_spare(self):
         return self.first_roll != MAX_PINS and self.score() == MAX_PINS
